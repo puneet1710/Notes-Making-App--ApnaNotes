0cd75f68b13ec74b7b37e496ff78ac7a258911f1
puneet1710 <ypuneet7171@gmail.com>
2022-02-07 17:55:36 +0530

    Updates

---
+++ precache-manifest.d8d73cab0399038f19f1b2d5464daf6b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9219902cb73e48f2a617dde6f7a5da32",
+    "url": "/Notes-Making-App--ApnaNotes/index.html"
+  },
+  {
+    "revision": "0e4a0c09a48d4c2555ef",
+    "url": "/Notes-Making-App--ApnaNotes/static/js/2.13e6c72f.chunk.js"
+  },
+  {
+    "revision": "1bec5510cd5a49d2b1a2",
+    "url": "/Notes-Making-App--ApnaNotes/static/js/main.5fe150a0.chunk.js"
+  },
+  {
+    "revision": "66b0774ab03dd17b3b3d",
+    "url": "/Notes-Making-App--ApnaNotes/static/js/runtime-main.9fbd5cac.js"
+  }
+]);
